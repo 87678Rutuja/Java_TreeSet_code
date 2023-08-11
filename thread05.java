e49f6199c6fd3092533451efcc04a95e5fe6ba84
87678Rutuja <132226247+87678Rutuja@users.noreply.github.com>
2023-08-11 16:58:52 +0530

    Add files via upload

---
+++ thread05.java
@@ -0,0 +1,28 @@
+class MyThread implements Runnable{
+        int num;
+        MyThread(int num){
+                this.num=num;
+        }
+        public void run(){
+                System.out.println(Thread.currentThread()+"start Thread"+ num);
+                dailyTask();
+                 System.out.println(Thread.currentThread()+"End Thread"+ num);
+        }
+        void dailyTask(){
+                try{
+                        Thread.sleep(8000);
+                }
+                catch(InterruptedException ie){
+                }
+        }
+}
+class ThreadpoolDemo{
+        public static void main(String[] ar){
+                Executorservices ser = Executors . new cachedThreadpool();
+                for(int i = 0; i<6;i++){
+                        MyThread obj = new MyThread(i);
+                        ser.execute(obj);
+                }
+                ser.shatdown();
+        }
+}
---
+++ treeset.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class TreeSetDemo{
+	public static void main(String[] ar){
+		TreeSet ts = new TreeSet();
+
+		ts.add("sara");
+		ts.add("alisha");
+		ts.add("samer");
+		ts.add("ruta");
+
+		System.out.println(ts);
+	}
+}
